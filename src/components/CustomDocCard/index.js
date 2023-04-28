93cf3bd4a5596243635a6035e5ca6b18db034a09
Nikolas LeBlanc <nikolas.leblanc@gmail.com>
2023-04-28 11:38:43 -0400

    adds integration page for rum integration

--- src/components/CustomDocCard/index.js
+++ src/components/CustomDocCard/index.js
@@ -1,34 +1,38 @@
-import React from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // Import the FontAwesomeIcon component.
-import { library } from '@fortawesome/fontawesome-svg-core'; // Import the library component.
-import { fab } from '@fortawesome/free-brands-svg-icons'; // Import all brands icons.
-import { fas } from '@fortawesome/free-solid-svg-icons'; // Import all solid icons.
-library.add(fab, fas); // Add all icons to the library so you can use them without importing them individually.
+import React from 'react'
+import clsx from 'clsx'
+import Link from '@docusaurus/Link'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome' // Import the FontAwesomeIcon component.
+import { library } from '@fortawesome/fontawesome-svg-core' // Import the library component.
+import { fab } from '@fortawesome/free-brands-svg-icons' // Import all brands icons.
+import { fas } from '@fortawesome/free-solid-svg-icons' // Import all solid icons.'
+import { faDataDogIcon } from '../CustomIcons/datadog'
+library.add(fab, fas, faDataDogIcon) // Add all icons to the library so you can use them without importing them individually.
 import {
   findFirstCategoryLink,
   useDocById,
-} from '@docusaurus/theme-common/internal';
-import isInternalUrl from '@docusaurus/isInternalUrl';
-import { translate } from '@docusaurus/Translate';
-import styles from './styles.module.css';
+} from '@docusaurus/theme-common/internal'
+import isInternalUrl from '@docusaurus/isInternalUrl'
+import { translate } from '@docusaurus/Translate'
+import styles from './styles.module.css'
+
 function CardContainer({ href, children }) {
   return (
     <Link
       href={href}
-      className={clsx('card padding--lg', styles.cardContainer)}>
+      className={clsx('card padding--lg', styles.cardContainer)}
+    >
       {children}
     </Link>
-  );
+  )
 }
 function CardLayout({ href, icon, iconSet, title, description }) {
   return (
     <CardContainer href={href}>
-
       <h2 className={clsx('text--truncate', styles.cardTitle)} title={title}>
-
-      <FontAwesomeIcon icon={iconSet ? [iconSet, icon] : icon} className="mr-2" />
+        <FontAwesomeIcon
+          icon={iconSet ? [iconSet, icon] : icon}
+          className="mr-2"
+        />
         <span className="ml-2">{title}</span>
       </h2>
 
@@ -40,18 +44,17 @@ function CardLayout({ href, icon, iconSet, title, description }) {
           {description}
         </p>
       )}
-
     </CardContainer>
-  );
+  )
 }
 function CardCategory({ item }) {
-  const href = findFirstCategoryLink(item);
-  
+  const href = findFirstCategoryLink(item)
+
   if (!href) {
-    return null;
+    return null
   }
 
-  const { customProps = {} } = item;
+  const { customProps = {} } = item
 
   return (
     <CardLayout
@@ -60,10 +63,10 @@ function CardCategory({ item }) {
       description={customProps.description}
       icon={customProps.icon}
     />
-  );
+  )
 }
 function CardLink({ item }) {
-  const doc = useDocById(item.docId ?? undefined);
+  const doc = useDocById(item.docId ?? undefined)
   return (
     <CardLayout
       href={item.href}
@@ -72,17 +75,16 @@ function CardLink({ item }) {
       iconSet={item.iconSet}
       description={doc?.description ?? item.description}
     />
-  );
+  )
 }
-
 
 export default function CustomDocCard({ item }) {
   switch (item.type) {
     case 'link':
-      return <CardLink item={item} />;
+      return <CardLink item={item} />
     case 'category':
-      return <CardCategory item={item} />;
+      return <CardCategory item={item} />
     default:
-      throw new Error(`unknown item type ${JSON.stringify(item)}`);
+      throw new Error(`unknown item type ${JSON.stringify(item)}`)
   }
 }
