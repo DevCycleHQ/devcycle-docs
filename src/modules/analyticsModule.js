e666bb317db488b057ba467aee8cb920b3a13725
Kenny Nguyen <19987735+kenny-nguyen@users.noreply.github.com>
2023-08-16 14:53:12 -0400

    swapped mixpanel doc tracking to rudderstack

--- src/modules/analyticsModule.js
+++ src/modules/analyticsModule.js
@@ -1,10 +1,7 @@
-import mixpanel from 'mixpanel-browser'
-mixpanel.init('a31a8987dc6ba699cbca6728fa482473', { debug: false })
-
 export function onRouteDidUpdate({ location, previousLocation }) {
   // Don't execute if we are still on the same page; the lifecycle may be fired
   // because the hash changes (e.g. when navigating between headings)
   if (location.pathname !== previousLocation?.pathname) {
-    mixpanel.track('Page View', { url: location.pathname })
+    window.rudderanalytics.track('Page View', { url: location.pathname })
   }
 }
