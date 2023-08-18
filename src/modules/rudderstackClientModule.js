8f4e194080941f7af8d54f56cbb79cadcf0b224a
Kenny Nguyen <19987735+kenny-nguyen@users.noreply.github.com>
2023-08-18 10:24:14 -0400

    added window is undefined check

--- src/modules/rudderstackClientModule.js
+++ src/modules/rudderstackClientModule.js
@@ -1,57 +1,59 @@
 !(function () {
-  var e = (globalThis.rudderanalytics = globalThis.rudderanalytics || [])
-  ;(e.methods = [
-    'load',
-    'page',
-    'track',
-    'identify',
-    'alias',
-    'group',
-    'ready',
-    'reset',
-    'getAnonymousId',
-    'setAnonymousId',
-    'getUserId',
-    'getUserTraits',
-    'getGroupId',
-    'getGroupTraits',
-    'startSession',
-    'endSession',
-  ]),
-    (e.factory = function (t) {
-      return function () {
-        var r = Array.prototype.slice.call(arguments)
-        return r.unshift(t), e.push(r), e
-      }
-    })
-  for (var t = 0; t < e.methods.length; t++) {
-    var r = e.methods[t]
-    e[r] = e.factory(r)
-  }
-  ;(e.loadJS = function (e, t) {
-    var r = document.createElement('script')
-    ;(r.type = 'text/javascript'),
-      (r.async = !0),
-      (r.src = 'https://analytics.devcycle.com/sdk')
-    var a = document.getElementsByTagName('script')[0]
-    a.parentNode.insertBefore(r, a)
-  }),
-    e.loadJS(),
-    e.load('2U4ReRYBAsyeoBkDlIW9P4CitLU', 'https://analytics.devcycle.com', {
-      configUrl: 'https://analytics.devcycle.com',
+  if (typeof window !== 'undefined') {
+    var e = (globalThis.rudderanalytics = globalThis.rudderanalytics || [])
+    ;(e.methods = [
+      'load',
+      'page',
+      'track',
+      'identify',
+      'alias',
+      'group',
+      'ready',
+      'reset',
+      'getAnonymousId',
+      'setAnonymousId',
+      'getUserId',
+      'getUserTraits',
+      'getGroupId',
+      'getGroupTraits',
+      'startSession',
+      'endSession',
+    ]),
+      (e.factory = function (t) {
+        return function () {
+          var r = Array.prototype.slice.call(arguments)
+          return r.unshift(t), e.push(r), e
+        }
+      })
+    for (var t = 0; t < e.methods.length; t++) {
+      var r = e.methods[t]
+      e[r] = e.factory(r)
+    }
+    ;(e.loadJS = function (e, t) {
+      var r = document.createElement('script')
+      ;(r.type = 'text/javascript'),
+        (r.async = !0),
+        (r.src = 'https://analytics.devcycle.com/sdk')
+      var a = document.getElementsByTagName('script')[0]
+      a.parentNode.insertBefore(r, a)
     }),
-    e.ready(() => {
-      var open = XMLHttpRequest.prototype.open
-      XMLHttpRequest.prototype.open = function () {
-        var method = arguments[0]
-        var url = arguments[1]
-        open.apply(this, [
-          method,
-          url === 'https://analytics.devcycle.com/v1/track'
-            ? 'https://analytics.devcycle.com/v1/t'
-            : url,
-        ])
-      }
-      globalThis.rudderanalytics.page()
-    })
+      e.loadJS(),
+      e.load('2U4ReRYBAsyeoBkDlIW9P4CitLU', 'https://analytics.devcycle.com', {
+        configUrl: 'https://analytics.devcycle.com',
+      }),
+      e.ready(() => {
+        var open = XMLHttpRequest.prototype.open
+        XMLHttpRequest.prototype.open = function () {
+          var method = arguments[0]
+          var url = arguments[1]
+          open.apply(this, [
+            method,
+            url === 'https://analytics.devcycle.com/v1/track'
+              ? 'https://analytics.devcycle.com/v1/t'
+              : url,
+          ])
+        }
+        globalThis.rudderanalytics.page()
+      })
+  }
 })()
