4a5198076e9ca48cf91afe04c249cedeac319296
kenny-nguyenn <developedbykenny@gmail.com>
2023-08-21 10:17:27 -0400

    swapped track method for page

--- src/modules/analyticsModule.js
+++ src/modules/analyticsModule.js
@@ -7,15 +7,10 @@ const client = new RudderAnalytics("2U4ReRYBAsyeoBkDlIW9P4CitLU", {
 export function onRouteDidUpdate({ location, previousLocation }) {
   // Don't execute if we are still on the same page; the lifecycle may be fired
   // because the hash changes (e.g. when navigating between headings)
-<<<<<<< HEAD
-  if (location.pathname !== previousLocation?.pathname) {
-    client.track('Page View', { url: location.pathname })
-=======
   if (
     location.pathname !== previousLocation?.pathname &&
     typeof window !== 'undefined'
   ) {
     window.rudderanalytics.page()
->>>>>>> 88988eaab53e344d9cb45a8085f37a8a786902b1
   }
 }
