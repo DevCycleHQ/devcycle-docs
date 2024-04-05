d8885b6c314794acea1f338fc563d179deb61dc4
DevCycle Automation <github-tracker-bot@taplytics.com>
2024-04-05 15:23:36 +0000

    Update VSCode Extension version to 1.4.4

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -45,7 +45,7 @@ const YouTubeTransformer = {
  */
 const DVC_CLI_VERSION = 'v5.14.3' // auto updated by dvc cli release workflow
 
-const VSCODE_EXTENSION_VERSION = 'v1.4.3' // auto updated by extension release workflow
+const VSCODE_EXTENSION_VERSION = 'v1.4.4' // auto updated by extension release workflow
 
 const removeDocsSections = (content, sectionNames, headerIdentifier = "##") => {
   let result = content
