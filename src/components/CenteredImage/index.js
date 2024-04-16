e817c2f547cdb2cefc7921eccd8e93621a049bbc
Adam Wootton <adam@taplytics.com>
2024-04-16 13:51:46 -0400

    fix scaling

--- src/components/CenteredImage/index.js
+++ src/components/CenteredImage/index.js
@@ -3,6 +3,6 @@ import styles from './styles.module.css'
 
 export function CenteredImage({ src, height, alt }) {
   return <div className={styles.centeredRow}>
-    <img src={src} height={height} alt={alt}/>
+    <img src={src} style={{maxHeight: `${height}px`}} alt={alt}/>
   </div>
 }
