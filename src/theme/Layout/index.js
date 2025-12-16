0164dee7ea9081253097f7292abaf9d85b3000b4
Jonathan Norris <jonathan@taplytics.com>
2025-12-16 14:12:44 -0500

    chore: remove @vercel/analytics package

--- src/theme/Layout/index.js
+++ src/theme/Layout/index.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import Layout from '@theme-original/Layout'
 import Head from '@docusaurus/Head'
-import { Analytics } from '@vercel/analytics/react'
 
 export default function LayoutWrapper(props) {
   return (
     <>
       <Layout {...props} />
-      <Analytics />
     </>
   )
 }
